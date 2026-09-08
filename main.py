4158359aa86342a01afd952abb500d7ca01a7cba
viveksaini9144-jpg <viveksaini9144@gmail.com>
2026-09-08 22:15:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://viveksaini9144-jpg.github.io"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def home():
